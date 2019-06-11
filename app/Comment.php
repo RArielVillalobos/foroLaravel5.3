6113f1364a3a9f4d181850028bf669b1112f18a3
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-06-11 01:07:59 -0300

    Revert "Revert "Add markAsAnswer method to the comment model""
    This reverts commit 67b0ae12b757330fd6a1d1c4c9b024135211429a.

--- app/Comment.php
+++ app/Comment.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+    //casteando la columna answrd para que sea booleana ya que en la bd se creo como tinint(0 y 1)
+    //pero nosotros le pasamos parametros true o false si no dara error al insertar valores
+    protected $casts=[
+        'answer'=>'boolean'
+    ];
     protected $fillable=['comment','post_id','user_id'];
 
     public function post(){
@@ -15,5 +20,25 @@ class Comment extends Model
 
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public function markAsAnswer(Post $post){
+
+
+
+
+            //si ya hay un comentario marcado como la respuesta del post, actualizarlo el comentario actual para que ya no sea la respuesta d post
+            $post->comments()->where('answer',true)->update(['answer'=>false]);
+
+            //$post->comments()->where('answer',true)->update(['answer'=>false]);
+
+            //marcar la columna como verdadera
+            $this->answer=true;
+            $this->save();
+
+            //el post ya no esta pendiente
+            $post->pending=false;
+            $post->save();
+
     }
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -27,7 +27,7 @@ $factory->define(\App\Post::class,function (\Faker\Generator $faker){
         return [
             'title'=>$faker->sentence,
             'content'=>$faker->paragraph,
-            'pending'=>$faker->boolean(),
+            'pending'=>true,
             //solamente se ejecutara si no estamos usando el usuario fuera del model factory
             'user_id'=>function (){
 
@@ -36,4 +36,21 @@ $factory->define(\App\Post::class,function (\Faker\Generator $faker){
 
             
         ];
+});
+
+$factory->define(\App\Comment::class,function(\Faker\Generator $faker){
+   return [
+       'comment'=>$faker->paragraph,
+       'answer'=>false,
+       //evitar que se ejecute en caso de que pase manualmente el id del post
+       'post_id'=>function(){
+            return factory(\App\Post::class)->create()->id;
+       },
+       'user_id'=>function (){
+
+           return factory(\App\User::class)->create()->id;
+       },
+
+
+   ];
 });
